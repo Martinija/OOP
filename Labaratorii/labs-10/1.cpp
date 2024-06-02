b001627846d1b5a6695d0ae0ebb91fd19be85218
Martina Gjorgjievska <martina.gjorgjievska@students.finki.ukim.mk>
2024-06-02 19:47:41 +0200

    Create 1.cpp

---
+++ Labaratorii/labs-10/1.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+
+using namespace std;
+template <typename T>
+vector<T>filter(T *array,int n, T el){
+    vector<T> result;
+    for (int i = 0; i <n ; ++i) {
+        if(array[i]<el){
+            result.push_back(array[i]);
+        }
+    }
+    return result;
+}
+
+//TODO your code here
+
+int main() {
+    int testCase;
+    cin >> testCase;
+
+    if (testCase == 1) {
+        // Test case 1: Filter integers
+        int intSize;
+        cin >> intSize;
+        int* intArray = new int[intSize];
+        for (int i = 0; i < intSize; i++) {
+            cin >> intArray[i];
+        }
+        int intThreshold;
+        cin >> intThreshold;
+        vector<int> intResult = filter(intArray, intSize, intThreshold);
+        for (int val : intResult) {
+            cout << val << " ";
+        }
+        cout << endl;
+
+        // Clean up dynamically allocated memory
+        delete[] intArray;
+    } else if (testCase == 2) {
+        // Test case 2: Filter strings
+        int strSize;
+        cin >> strSize;
+        string* strArray = new string[strSize];
+        for (int i = 0; i < strSize; i++) {
+            cin >> strArray[i];
+        }
+        string strThreshold;
+        cin >> strThreshold;
+        vector<string> strResult = filter(strArray, strSize, strThreshold);
+        for (string val : strResult) {
+            cout << val << " ";
+        }
+        cout << endl;
+
+        // Clean up dynamically allocated memory
+        delete[] strArray;
+    }
+
+    return 0;
+}
